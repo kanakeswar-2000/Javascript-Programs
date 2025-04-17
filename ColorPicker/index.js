b403c17eb7e2de25b39205ee7a43cd1d4e1d55b2
kanakeswar-2000 <pkanakeswarareddy@gmail.com>
2025-04-17 19:00:46 +0530

    initial commit

---
+++ ColorPicker/index.js
@@ -0,0 +1,19 @@
+function b1() {
+    document.getElementById("selectedColorHexCode").textContent = "#e0e0e0";
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#e0e0e0";
+}
+
+function b2() {
+    document.getElementById("selectedColorHexCode").textContent = "#6fcf97";
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf97";
+}
+
+function b3() {
+    document.getElementById("selectedColorHexCode").textContent = "#56ccf2";
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#56ccf2";
+}
+
+function b4() {
+    document.getElementById("selectedColorHexCode").textContent = "#bb6bd9";
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#bb6bd9";
+}
