7f8710ad1f28851c0e948a1263d103e6e5caec6d
kanakeswar-2000 <pkanakeswarareddy@gmail.com>
2025-04-20 17:10:01 +0530

    initial commit

---
+++ AddGame/index.js
@@ -0,0 +1,31 @@
+let firstnumberElement = document.getElementById("firstNumber");
+let secondnumberElement = document.getElementById("secondNumber");
+
+let userInputElement = document.getElementById("userInput");
+let gameresultElement = document.getElementById("gameResult");
+
+function check_result() {
+    let firstnumber = parseInt(firstnumberElement.textContent);
+
+    let sum = parseInt(userInputElement.value);
+
+    let secondnumber = parseInt(secondnumberElement.textContent);
+
+    if (firstnumber + secondnumber === sum) {
+        gameresultElement.textContent = "Congratulations! You got it right";
+        gameresultElement.style.backgroundColor = "#028a0f"
+    } else {
+        gameresultElement.textContent = "Please Try Again!";
+        gameresultElement.style.backgroundColor = "#1e217c"
+    }
+}
+
+function restart() {
+    firstnumberElement.textContent = Math.ceil(Math.random() * 100);
+
+    secondnumberElement.textContent = Math.ceil(Math.random() * 100);
+
+    gameresultElement.textContent = "";
+    userInputElement.value = "";
+}
+restart();
